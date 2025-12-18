0ca0dda703562c1e713688e9f9caf36d5b24d624
jqln <jacqueline.amherst@icloud.com>
2025-12-17 22:39:02 -0600

    fix: correct comment s/piano/violin/

--- source/audio/fluidsynthClient.ts
+++ source/audio/fluidsynthClient.ts
@@ -26,7 +26,7 @@ export class FluidSynthClient {
 		return new Promise((resolve: () => void, reject: (err: Error) => void) => {
 			this.socket = net.createConnection({ host: this.host, port: this.port }, () => {
 				console.log('[FluidSynthClient]: Connected');
-				this.sendLine('prog 0 40'); // set the instrument to piano
+				this.sendLine('prog 0 40'); // set the instrument to violin
 				resolve();
 			});
 			this.socket.once("error", (err) => {
