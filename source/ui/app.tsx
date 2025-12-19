3316d6ca660cb602b9fb20aab3bf42df9fd379ed
jqln <jacqueline.amherst@icloud.com>
2025-12-18 20:12:13 -0600

    feat: add sidebar and upperwindow styling

--- source/ui/app.tsx
+++ source/ui/app.tsx
@@ -1,9 +1,39 @@
+import { useState } from 'react';
 import { Text, Box } from 'ink';
+import SelectInput from 'ink-select-input';
 import { IFluidSynthClient } from '../audio/fluidsynthClient.js';
 
-function Sidebar() {
+interface Item {
+	label: string
+	value: string
+}
+
+interface SidebarProps {
+	onSelect(item: Item): void
+}
+
+interface UpperWindowProps {
+	content: string
+}
+
+const sidebarItems: Item[] = [
+	{ label: 'sidebarItem1', value: 'Sidebar Item 1' },
+	{ label: 'sidebarItem2', value: 'Sidebar Item 2' },
+	{ label: 'synth', value: 'Synth' }
+]
+
+function Sidebar({ onSelect }: SidebarProps) {
 	return (
-		<Box flexDirection='column' width={50}>
+		<Box flexDirection='column' width={40} height={'100%'} borderStyle={'double'} borderColor={'blue'} paddingLeft={3} paddingRight={3} paddingTop={1} >
+			<SelectInput items={sidebarItems} onSelect={onSelect} />
+		</Box>
+	)
+}
+
+function UpperWindow({ content }: UpperWindowProps) {
+	return (
+		<Box flexDirection='row' width={'100%'} height={'100%'} borderStyle={'double'} borderColor={'blue'} paddingLeft={3} paddingRight={3} paddingTop={1} justifyContent='center'>
+			<Text>{content}</Text>
 		</Box>
 	)
 }
@@ -13,9 +43,22 @@ interface AppProps {
 }
 
 export default function App({ synth }: AppProps) {
+	const defaultContent = "No Selection";
+	const [currentContent, setCurrentContent] = useState<string>(defaultContent);
+
+	const handleSidebarSelect = (item: Item): void => {
+		if (item.label === 'synth') {
+			synth.playNote({ name: 'C', octave: 4 });
+			setCurrentContent('Playing Note: C 4')
+		} else {
+			setCurrentContent(item.value);
+		}
+	}
+
 	return (
 		<Box>
-			<Sidebar />
+			<Sidebar onSelect={handleSidebarSelect} />
+			<UpperWindow content={currentContent} />
 		</Box>
 	);
 }
